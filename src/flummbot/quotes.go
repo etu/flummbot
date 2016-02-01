46a6ceea7925586912b195c1d251250f9d7c8761
Elis Axelsson <elis.axelsson@gmail.com>
2016-02-01 20:46:44 +0100

    Added Channel to Quotes table

--- src/flummbot/quotes.go
+++ src/flummbot/quotes.go
@@ -11,10 +11,11 @@ type Quotes struct {
 func (q *Quotes) DbSetup(db *sql.DB) {
 	db.Exec(`
 		CREATE TABLE IF NOT EXISTS quotes (
-			"id"    integer NOT NULL PRIMARY KEY,
-			"nick"  text,
-			"quote" text,
-			"date"  text
+			"id"      integer NOT NULL PRIMARY KEY,
+			"nick"    text,
+			"quote"   text,
+			"channel" text,
+			"date"    text
 		);
 	`)
 }
