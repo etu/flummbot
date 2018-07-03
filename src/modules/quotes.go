7e811ee5a3249497f82fa1365912021ef5897ef1
Elis Hirwing <elis@hirwing.se>
2018-07-03 16:03:55 +0200

    Handle empty databases for quotes

--- src/modules/quotes.go
+++ src/modules/quotes.go
@@ -51,17 +51,21 @@ func (q *Quotes) handle(c *irc.IrcConnection, e *ircevent.Event) {
 				Channel: e.Arguments[0],
 			}).Order("RANDOM()").First(&quote)
 
-			// Format the timestamp
-			date := quote.CreatedAt.Format("2006-01-02 15:04:05")
+			// If we have a real quote returned, print it, otherwise skip it.
+			// Not having one is probably because of an empty database.
+			if quote.ID != 0 {
+				// Format the timestamp
+				date := quote.CreatedAt.Format("2006-01-02 15:04:05")
 
-			// Add zero-with-space between first and second letter to break
-			// highlighting of the creator when printing a quote.
-			spacedNick := quote.Nick[0:1] + "\u200B" + quote.Nick[1:]
+				// Add zero-with-space between first and second letter to break
+				// highlighting of the creator when printing a quote.
+				spacedNick := quote.Nick[0:1] + "\u200B" + quote.Nick[1:]
 
-			c.IrcEventConnection.Privmsg(
-				e.Arguments[0],
-				"Quote added by "+spacedNick+" @ "+date+": "+quote.Body,
-			)
+				c.IrcEventConnection.Privmsg(
+					e.Arguments[0],
+					"Quote added by "+spacedNick+" @ "+date+": "+quote.Body,
+				)
+			}
 
 		} else { // Add quote to database
 			q.Db.Gorm.Create(&QuotesModel{
