f00d0fa432fcb3554ea02ae385c163ef9124fc62
Elis Hirwing <elis@hirwing.se>
2020-11-09 18:40:42 +0100

    flake-modules: Introduce a flake module

--- flake.nix
+++ flake.nix
@@ -25,5 +25,68 @@
       inherit system;
       overlays = [ self.overlay ];
     }).flummbot);
+
+    # Set up flake module
+    nixosModule = { inputs, options, modulesPath, config, lib }: let
+      cfg = config.services.flummbot;
+    in {
+      # Set up module options
+      options.services.flummbot = {
+        enable = lib.mkEnableOption "Small IRC bot in go used for my channels";
+
+        user = lib.mkOption {
+          type = lib.types.str;
+          default = "flummbot";
+          defaultText = "flummbot";
+          description = "Username of user running the bot software";
+        };
+
+        group = lib.mkOption {
+          type = lib.types.str;
+          default = "flummbot";
+          defaultText = "flummbot";
+          description = "Group of user running the bot software";
+        };
+
+        package = lib.mkOption {
+          type = lib.types.package;
+          default = self.defaultPackage.x86_64-linux;
+          defaultText = "The package to use";
+        };
+
+        stateDirectory = lib.mkOption {
+          type = lib.types.str;
+          default = "/var/lib/flummbot";
+          defaultText = "/var/lib/flummbot";
+          description = "State directory of flummbot";
+        };
+      };
+
+      # Set up module implementation
+      config = lib.mkIf cfg.enable {
+        users.users."${cfg.user}" = {
+          description = "System user for flummbot";
+          isSystemUser = true;
+          inherit (cfg) group;
+        };
+        users.groups."${cfg.group}" = {};
+
+        systemd.tmpfiles.rules = [ "d ${cfg.stateDirectory} 0700 ${cfg.user} ${cfg.group} -" ];
+
+        systemd.services.flummbot = {
+          description = "flummbot irc bot";
+          after = [ "network.target" "network-online.target" ];
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            Type = "simple";
+            User = cfg.user;
+            Group = cfg.group;
+            ExecStart = "${cfg.package}/bin/flummbot --config ${cfg.stateDirectory}/flummbot.toml";
+            WorkingDirectory = cfg.stateDirectory;
+            Restart = "always";
+          };
+        };
+      };
+    };
   };
 }
